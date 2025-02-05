247f845490c7eb98414227c810bf91c19d897507
Anthony Bouch <anthony@infonomic.io>
2025-02-05 15:39:55 +0700

    feat: simplified typography

---
+++ packages/uikit/src/styles/typography/code.stories.tsx
@@ -0,0 +1,69 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+
+import { Highlight, themes } from 'prism-react-renderer'
+
+const code = `const x = 'foo';
+function printFoo() {
+  console.log(x);
+}
+printFoo();`
+
+type CodeIntrinsicProps = React.JSX.IntrinsicElements['pre']
+interface CodeProps extends CodeIntrinsicProps {
+  className?: string
+  title?: string
+  code: string
+  language?: string
+}
+
+function CodeDemo({ code, className, language = 'jsx' }: CodeProps): React.JSX.Element {
+  return (
+    <Highlight theme={themes.nightOwl} code={code} language={language}>
+      {({ tokens, getLineProps, getTokenProps }) => (
+        <pre style={{ backgroundColor: 'var(--canvas-800)' }} className={className}>
+          {tokens.map((line, i) => {
+            const lineProps = getLineProps({ line, key: i })
+            return (
+              <div {...lineProps} key={i} style={{ lineHeight: 1.3 }}>
+                <span style={{ marginLeft: '-8px', marginRight: '12px' }}>{i + 1}</span>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token })} />
+                ))}
+              </div>
+            )
+          })}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
+
+export default {
+  title: 'Typography',
+  argTypes: {}
+}
+
+export const Code = (): React.JSX.Element => {
+  return (
+    <>
+      <div style={{ marginBottom: '48px' }}>
+        <div style={{ maxWidth: '700px', margin: 'auto' }} className="prose">
+          <h1>Heading 1</h1>
+          <p>
+            This is a <code>inline code here</code> of text that we&apos;ll use for our typography
+            and code storybook page. This is a paragraph of text that we&apos;ll use for our
+            typography and code storybook page. Here is a code block...
+          </p>
+          <CodeDemo code={code} />
+          <p>
+            This is a paragraph of text that we&apos;ll use for our typography storybook page. This
+            is a paragraph of text that we&apos;ll use for our typography storybook page. This is a
+            paragraph of text that we&apos;ll use for our typography storybook page. This is a
+            paragraph of text that we&apos;ll use for our typography storybook page.
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
