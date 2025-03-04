e59531098492a8b770432118c81b72e784ffcf84
Anthony Bouch <anthony@infonomic.io>
2025-03-04 09:52:44 +0700

    feat: card polymophic and test src imports

---
+++ packages/uikit/src/astro.ts
@@ -0,0 +1,25 @@
+import ButtonComponent from './components/button/button.astro'
+import CardContentComponent from './components/card/card-content.astro'
+import CardDescriptionComponent from './components/card/card-description.astro'
+import CardFooterComponent from './components/card/card-footer.astro'
+import CardHeaderComponent from './components/card/card-header.astro'
+import CardTitleComponent from './components/card/card-title.astro'
+import CardComponent from './components/card/card.astro'
+import ContainerComponent from './components/container/container.astro'
+import SectionComponent from './components/section/section.astro'
+import IconElementComponent from './icons/icon-element.astro'
+import LightIconComponent from './icons/light-icon.astro'
+import MoonIconComponent from './icons/moon-icon.astro'
+
+export const Button = ButtonComponent
+export const CardContent = CardContentComponent
+export const CardDescription = CardDescriptionComponent
+export const CardFooter = CardFooterComponent
+export const CardHeader = CardHeaderComponent
+export const CardTitle = CardTitleComponent
+export const Card = CardComponent
+export const Container = ContainerComponent
+export const Section = SectionComponent
+export const IconElement = IconElementComponent
+export const LightIcon = LightIconComponent
+export const MoonIcon = MoonIconComponent
