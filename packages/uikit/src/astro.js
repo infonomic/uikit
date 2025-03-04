9c1a667f470a6aeabe1a22452fbf2c6b593627e0
Anthony Bouch <anthony@infonomic.io>
2025-03-04 18:05:34 +0700

    feat: input with adornments

--- packages/uikit/src/astro.js
+++ packages/uikit/src/astro.js
@@ -12,9 +12,11 @@ import InputAdornmentComponent from './components/input/input-adornment.astro'
 import InputComponent from './components/input/input.astro'
 import LabelComponent from './components/input/label.astro'
 import SectionComponent from './components/section/section.astro'
+import CloseIconComponent from './icons/close-icon.astro'
 import IconElementComponent from './icons/icon-element.astro'
 import LightIconComponent from './icons/light-icon.astro'
 import MoonIconComponent from './icons/moon-icon.astro'
+import SearchIconComponent from './icons/search-icon.astro'
 
 export const Button = ButtonComponent
 export const CardContent = CardContentComponent
@@ -33,3 +35,5 @@ export const Section = SectionComponent
 export const IconElement = IconElementComponent
 export const LightIcon = LightIconComponent
 export const MoonIcon = MoonIconComponent
+export const SearchIcon = SearchIconComponent
+export const CloseIcon = CloseIconComponent
