36b432b31649eb862e147af2e369c76ebf1ce280
Anthony Bouch <anthony@infonomic.io>
2025-03-04 10:29:41 +0700

    feat: more work on components

--- packages/uikit/src/components/card/card.tsx
+++ packages/uikit/src/components/card/card.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import cx from 'classnames'
+import * as React from 'react'
 import styles from './card.module.css'
 
 export type AsDiv = {
