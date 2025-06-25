8bcd828424fa96ec101d3075fe7c069284fb881f
Anthony Bouch <anthony@infonomic.io>
2025-06-25 17:32:43 +0700

    feat: radio group

--- packages/uikit/src/components/input/radio-group.stories.tsx
+++ packages/uikit/src/components/input/radio-group.stories.tsx
@@ -10,19 +10,57 @@ export default {
   argTypes: {},
 }
 
-const values: RadioGroupValue[] = [
+const values1: RadioGroupValue[] = [
   { id: 'rg1', label: 'One', value: '1' },
   { id: 'rg2', label: 'Two', value: '2' },
   { id: 'rg3', label: 'Three', value: '3' },
+]
+
+const values2: RadioGroupValue[] = [
+  { id: 'rg4', label: 'One', value: '1' },
+  { id: 'rg5', label: 'Two', value: '2' },
+  { id: 'rg6', label: 'Three', value: '3' },
+]
+
+const values3: RadioGroupValue[] = [
+  { id: 'rg4', label: 'One', value: '1' },
+  { id: 'rg5', label: 'Two', value: '2' },
+  { id: 'rg6', label: 'Three', value: '3' },
 ]
 
 export const Default = (): React.JSX.Element => {
   return (
     <>
       <div style={{ maxWidth: '600px', margin: '2rem auto' }}>
-        <RadioGroup>
-          {values.map((value) => (
+        <RadioGroup direction="row">
+          {values1.map((value) => (
             <RadioGroupItem key={value.id} value={value.value} id={value.id} label={value.label} />
+          ))}
+        </RadioGroup>
+      </div>
+      <div style={{ maxWidth: '600px', margin: '2rem auto' }}>
+        <RadioGroup direction="row">
+          {values2.map((value) => (
+            <RadioGroupItem
+              intent="secondary"
+              key={value.id}
+              value={value.value}
+              id={value.id}
+              label={value.label}
+            />
+          ))}
+        </RadioGroup>
+      </div>
+      <div style={{ maxWidth: '600px', margin: '2rem auto' }}>
+        <RadioGroup direction="column">
+          {values3.map((value) => (
+            <RadioGroupItem
+              intent="success"
+              key={value.id}
+              value={value.value}
+              id={value.id}
+              label={value.label}
+            />
           ))}
         </RadioGroup>
       </div>
--- packages/uikit/src/components/input/radio-group.tsx
+++ packages/uikit/src/components/input/radio-group.tsx
@@ -1,5 +1,7 @@
+import cx from 'classnames'
 import { RadioGroup as RadioGroupPrimitive } from 'radix-ui'
 import type React from 'react'
+import type { Intent } from '../@types/shared'
 import styles from './radio-group.module.css'
 
 export interface RadioGroupValue {
@@ -9,36 +11,56 @@ export interface RadioGroupValue {
 }
 
 export const RadioGroupItem = ({
-  ref: forwardedRef,
+  intent = 'primary',
   className,
   id,
   value,
   label,
+  ref: forwardedRef,
   ...props
 }: RadioGroupPrimitive.RadioGroupItemProps & {
+  intent?: Intent
+  className?: string
+  id: string
+  value: string
   label: string
   ref?: React.RefObject<React.ComponentRef<'div'>>
 }) => {
   return (
-    <div ref={forwardedRef} style={{ display: 'flex', alignItems: 'center' }}>
-      <RadioGroupPrimitive.Item {...props} className={styles.Item} value={value} id={id}>
-        <RadioGroupPrimitive.Indicator className={styles.Indicator} />
+    <div ref={forwardedRef} className={styles['item-container']}>
+      <RadioGroupPrimitive.Item
+        {...props}
+        className={cx(styles.item, styles[intent])}
+        value={value}
+        id={id}
+      >
+        <RadioGroupPrimitive.Indicator className={styles.indicator} />
       </RadioGroupPrimitive.Item>
-      <label className={styles.Label} htmlFor={id}>
+      <label className={styles.label} htmlFor={id}>
         {label}
       </label>
     </div>
   )
 }
 
-export const RadioGroup = ({ children }: { children: React.ReactNode }) => (
-  <form>
-    <RadioGroupPrimitive.Root
-      className={styles.Root}
-      defaultValue="default"
-      aria-label="View density"
-    >
-      {children}
-    </RadioGroupPrimitive.Root>
-  </form>
+export const RadioGroup = ({
+  ref: forwardedRef,
+  className,
+  direction = 'column',
+  children,
+  ...props
+}: RadioGroupPrimitive.RadioGroupProps & {
+  direction?: 'row' | 'column'
+  className?: string
+  children: React.ReactNode
+  ref?: React.RefObject<React.ComponentRef<'div'>>
+}) => (
+  <RadioGroupPrimitive.Root
+    ref={forwardedRef}
+    className={styles[direction]}
+    // aria-label="View density"
+    {...props}
+  >
+    {children}
+  </RadioGroupPrimitive.Root>
 )
