ef3d93411e6211a15ebb7c8da986248988e909b8
Anthony Bouch <anthony@infonomic.io>
2025-07-02 09:24:00 +0700

    feat: updated avatar and updated tsconfig

--- packages/uikit/src/components/avatar/avatar.tsx
+++ packages/uikit/src/components/avatar/avatar.tsx
@@ -1,19 +1,18 @@
 'use client'
 
-import type React from 'react'
-
 import { Avatar as AvatarPrimitive } from 'radix-ui'
+import type React from 'react'
 
 import styles from './avatar.module.css'
 
 const url = 'https://picsum.photos/150'
 
-export const Avatar = (): React.JSX.Element => {
+export const Avatar = ({ initials }: { initials: string }): React.JSX.Element => {
   return (
     <AvatarPrimitive.Root className={styles['avatar-root']}>
       <AvatarPrimitive.Image src={url} alt="Avatar" className={styles['avatar-image']} />
       <AvatarPrimitive.Fallback className={styles['avatar-fallback']} delayMs={600}>
-        <span className={styles['avatar-text']}>AB</span>
+        <span className={styles['avatar-text']}>{initials}</span>
       </AvatarPrimitive.Fallback>
     </AvatarPrimitive.Root>
   )
