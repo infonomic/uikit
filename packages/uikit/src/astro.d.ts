22c7762315229632a052d782ff3a559abdb90a9d
Anthony Bouch <anthony@infonomic.io>
2025-08-22 07:52:19 +0700

    chore: fixed up astro exports and declaration files

---
+++ packages/uikit/src/astro.d.ts
@@ -0,0 +1,43 @@
+import type ButtonComponent from './components/button/button.astro'
+import type IconButtonComponent from './components/button/icon-button.astro'
+import type CardComponent from './components/card/card.astro'
+import type CardContentComponent from './components/card/card-content.astro'
+import type CardDescriptionComponent from './components/card/card-description.astro'
+import type CardFooterComponent from './components/card/card-footer.astro'
+import type CardHeaderComponent from './components/card/card-header.astro'
+import type CardTitleComponent from './components/card/card-title.astro'
+import type ContainerComponent from './components/container/container.astro'
+import type HamburgerComponent from './components/hamburger/hamburger.astro'
+import type ErrorTextComponent from './components/input/error-text.astro'
+import type HelpTextComponent from './components/input/help-text.astro'
+import type InputComponent from './components/input/input.astro'
+import type InputAdornmentComponent from './components/input/input-adornment.astro'
+import type LabelComponent from './components/input/label.astro'
+import type SectionComponent from './components/section/section.astro'
+import type CloseIconComponent from './icons/close-icon.astro'
+import type IconElementComponent from './icons/icon-element.astro'
+import type LightIconComponent from './icons/light-icon.astro'
+import type MoonIconComponent from './icons/moon-icon.astro'
+import type SearchIconComponent from './icons/search-icon.astro'
+
+export declare const Button: typeof ButtonComponent
+export declare const Card: typeof CardComponent
+export declare const CardContent: typeof CardContentComponent
+export declare const CardDescription: typeof CardDescriptionComponent
+export declare const CardFooter: typeof CardFooterComponent
+export declare const CardHeader: typeof CardHeaderComponent
+export declare const CardTitle: typeof CardTitleComponent
+export declare const CloseIcon: typeof CloseIconComponent
+export declare const Container: typeof ContainerComponent
+export declare const ErrorText: typeof ErrorTextComponent
+export declare const Hamburger: typeof HamburgerComponent
+export declare const HelpText: typeof HelpTextComponent
+export declare const IconButton: typeof IconButtonComponent
+export declare const IconElement: typeof IconElementComponent
+export declare const Input: typeof InputComponent
+export declare const InputAdornment: typeof InputAdornmentComponent
+export declare const Label: typeof LabelComponent
+export declare const LightIcon: typeof LightIconComponent
+export declare const MoonIcon: typeof MoonIconComponent
+export declare const SearchIcon: typeof SearchIconComponent
+export declare const Section: typeof SectionComponent
--- packages/uikit/src/components/card/card.tsx
+++ packages/uikit/src/components/card/card.tsx
@@ -1,7 +1,6 @@
-import type React from 'react'
-
 import { Slot } from '@radix-ui/react-slot'
 import cx from 'classnames'
+import type React from 'react'
 
 import styles from './card.module.css'
 
