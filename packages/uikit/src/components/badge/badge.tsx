f29d2b4ceb324520a6441e953c192ff1cf07d0db
Anthony Bouch <anthony@infonomic.io>
2025-09-14 19:53:28 +0700

    feat: complete implementation of Badge

--- packages/uikit/src/components/badge/badge.tsx
+++ packages/uikit/src/components/badge/badge.tsx
@@ -1,7 +1,34 @@
+import { Slot } from '@radix-ui/react-slot'
 import cx from 'classnames'
 import { Intent } from '../@types/shared'
 import styles from './badge.module.css'
 
-export function Badge({ children, intent = "primary" }: { children: React.ReactNode, intent?: Intent }): React.JSX.Element {
-  return <div className={cx('badge', intent, styles.badge, styles[intent])}>{children}</div>
+export type AsDiv = {
+  asChild?: false
+} & React.ComponentPropsWithoutRef<'div'>
+
+export interface AsSlot {
+  asChild?: true
+}
+
+export type BadgeProps<C extends React.ElementType = 'div'> = {
+  children: React.ReactNode
+  intent?: Intent
+  className?: string
+  asChild?: boolean
+} & (AsSlot | AsDiv)
+
+export function Badge({ 
+  className,
+  intent = 'primary',
+  children,
+  asChild,
+  ...rest }: BadgeProps): React.JSX.Element {
+   const Comp: React.ElementType = asChild === true ? Slot : 'div'
+  return (
+    <Comp 
+      className={cx('badge', intent, styles.badge, styles[intent], className)} {...rest}>
+      {children}
+    </Comp>
+  )
 }
